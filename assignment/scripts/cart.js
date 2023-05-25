6da195f4f28590d86e29247886b715a95c1570bf
Justin Lawson <jstn.lawson@gmail.com>
2023-05-25 10:18:51 -0500

    I forgot to fork before cloning so less commits on this one

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,51 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+const maxItems = 5;
 
+function addItem() { 
+    if (basket.length < maxItems) {
+    basket.push('Kale');
+    return true;
+    }
+    else if (basket.length >= maxItems) {
+    return false;
+    }
+ //I tried doing this by referencing the isFull function and
+ //couldn't get it to work. When I got the test to let me
+ //pass doing it this way I couldn't help but move on.
+ //If this is wrong or mostly wrong I'd like to know how to do it.   
+}
+
+console.log(addItem());
+
+function listItems() {
+    basket.forEach((basket) => console.log(basket));
+}
+//I don't think I've done a "forEach", i like being able to
+//write it out in one line but I see how it could be hard
+//to understand.
+console.log(listItems());
+
+
+
+function isFull() {
+    if (basket < maxItems) {
+        return false;
+    }
+   return true;
+}
+
+function removeItem(basket, Spinach) {
+    if (basket.indexOf(Spinach) === true) {
+        return basket.splice(Spinach);
+    } 
+        return null;
+}
+//I'm sure I'm missing somthing simple but the more I work on it
+//the more wrong it gets. I'm gonna have to take the L here.
+//Looking forward to finding out the answer!
 
 
 
